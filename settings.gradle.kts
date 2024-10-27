0ac896d8a7673c71a6966d7c372204ad4303447f
ANUKGUERRA <anukguerra@gmail.com>
2024-10-27 22:35:01 +0100

    Finalitzat

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Anuk_GuerraIzquierdo_UF1_ACT8"
+include(":app")
